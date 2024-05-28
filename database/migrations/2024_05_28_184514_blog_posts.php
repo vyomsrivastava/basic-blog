83cfed4b20161af8a63547b762bd97acfda8d7c6
Vyom <vyom@refinery89.com>
2024-05-29 00:25:03 +0530

    chore: adding migration for article table

---
+++ database/migrations/2024_05_28_184514_blog_posts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('is_public')->default(true);
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('blog');
+    }
+};
