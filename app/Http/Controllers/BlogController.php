c303fe0dd60c6945543e790855aa60047f9207d0
Vyom <vyom@refinery89.com>
2024-05-29 00:52:25 +0530

    chore: adding header and footer

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function returnBlogList(){
-        return view('index');
+        $allBlogPosts = Blog::select('title', 'updated_at', 'id')->get();
+        
+        return view('index')
+            ->with('posts', $allBlogPosts);
     }
 }
