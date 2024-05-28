9ee9b623d4e397958e87146f5a1b1c08c562ef12
Vyom <vyom@refinery89.com>
2024-05-29 01:11:43 +0530

    chore: adding admin login page

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,21 @@
+@include('admin.includes.header')
+  <form>
+    <img class="mb-4" src="https://geekyhumans.com/wp-content/uploads/2022/09/owl.png" alt="" width="150" height="150">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+  </form>
+</main>
+
+@include('includes.footer')
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [BlogController::class, 'returnBlogList'])->name('homepage');
+
+Route::get('/login', [AdminController::class, 'returnLoginPage'])->name('loginPage');
