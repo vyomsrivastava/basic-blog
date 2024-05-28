23c163a0aefb813c15ec8e9d9ca0695350302058
Vyom <vyom@refinery89.com>
2024-05-29 01:02:37 +0530

    chore: adding category

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -114,7 +114,7 @@
 <main>
 
 
-    <header class="p-1 text-bg-dark">
+    <header class="p-3 text-bg-dark">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
